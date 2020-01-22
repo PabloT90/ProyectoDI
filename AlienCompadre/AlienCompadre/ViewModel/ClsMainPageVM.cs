382ca6c976f66f90447179d7f20b5a20b360d9b5
Yeray Campanario <thewalkerofdreams@gmail.com>
2020-01-22 11:47:44 +0100

    AlienCompadre Mod

---
+++ AlienCompadre/AlienCompadre/ViewModel/ClsMainPageVM.cs
@@ -0,0 +1,61 @@
+﻿using AlienCompadre_Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienCompadre.ViewModel
+{
+    public class ClsMainPageVM : INotifyPropertyChanged
+    {
+        private ClsTablero _mazmorra;
+        private String _playerSrcImage;
+
+        #region Constructores
+        public ClsMainPageVM()
+        {
+            _mazmorra = new ClsTablero();
+            _playerSrcImage = "direccionImagen";
+        }
+        #endregion
+
+        #region Propiedades Públicas
+        public ClsTablero Mazmorra
+        {
+            get
+            {
+                return _mazmorra;
+            }
+            set
+            {
+                _mazmorra = value;
+                NotifyPropertyChanged("Mazmorra");
+            }
+        }
+
+        public String PlayerSrcImage
+        {
+            get
+            {
+                return _playerSrcImage;
+            }
+            set
+            {
+                _playerSrcImage = value;
+                NotifyPropertyChanged("PlayerSrcImage");
+            }
+        }
+        #endregion
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
