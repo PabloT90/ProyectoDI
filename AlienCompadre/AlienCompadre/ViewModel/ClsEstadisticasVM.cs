50761e09479cc13e7be5d517303fc7198dc12804
Pablo <36712068+PabloT90@users.noreply.github.com>
2020-02-19 13:45:55 +0100

    donete

---
+++ AlienCompadre/AlienCompadre/ViewModel/ClsEstadisticasVM.cs
@@ -0,0 +1,32 @@
+﻿using AlienCompadre_Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienCompadre.ViewModel {
+    public class ClsEstadisticasVM {
+
+        #region Propiedades privadas
+        private List<ClsStats> listaEstadisticas;
+        #endregion
+
+        #region constructor por defecto
+        public ClsEstadisticasVM() {
+            listaEstadisticas = new List<ClsStats>();
+        }
+        #endregion
+
+        #region Propiedades publicas
+        public List<ClsStats> Estadisticas {
+            get {
+                return listaEstadisticas;
+            }
+            set {
+                listaEstadisticas = value;
+            }
+        }
+        #endregion
+    }
+}
---
+++ AlienCompadre/AlienCompadre/ViewModel/ClsPantallaFinalVM.cs
@@ -0,0 +1,31 @@
+﻿using AlienCompadre_Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienCompadre.ViewModel {
+    public class ClsPantallaFinalVM {
+        #region Propiedades privadas
+        private ClsStats estadisticas;
+        #endregion
+
+        #region constructor por defecto
+        public ClsPantallaFinalVM() {
+            estadisticas = new ClsStats();
+        }
+        #endregion
+
+        #region Propiedades publicas
+        public ClsStats Estadicticas {
+            get {
+                return estadisticas;
+            }
+            set {
+                estadisticas = value;
+            }
+        }
+        #endregion
+    }
+}
